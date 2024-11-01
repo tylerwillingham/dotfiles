3d87f5701c1fbef860ae1440cedb39763371ebd1
Tyler Willingham <171991+tylerwillingham@users.noreply.github.com>
2024-11-01 15:23:47 -0700

    Install neovim with linux install script

--- linux-install.sh
+++ linux-install.sh
@@ -5,3 +5,6 @@ apt-get update
 echo "--- Installing zsh and oh-my-zsh"
 apt-get install -y zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+echo "--- Installing neovim"
+apt-get install -y neovim
