610ff35551c307ab5542acf5622d5fdcb6d9b48a
Tyler Willingham <171991+tylerwillingham@users.noreply.github.com>
2024-11-01 15:19:23 -0700

    ♻️ Improve install scripts with os-specific versions

---
+++ linux-install.sh
@@ -0,0 +1,7 @@
+echo "========== linux detected =========="
+
+apt-get update
+
+echo "--- Installing zsh and oh-my-zsh"
+apt-get install -y zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
